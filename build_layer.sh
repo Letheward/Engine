ce86ce270931cf50b3de5af4001739a191c7098c
Letheward <letheward@foxmail.com>
2022-06-14 20:17:02 +0800

    build script

---
+++ build_layer.sh
@@ -0,0 +1,9 @@
+# Windows + MinGW
+name="win32.o"
+src="src/layer/win32.c"
+opt="-O3"
+def="-D win32_layer_implementation"
+etc="-std=c99 -Wall -pedantic"
+
+# build
+gcc $src $obj $opt $def $etc -c -o lib/object/$name
